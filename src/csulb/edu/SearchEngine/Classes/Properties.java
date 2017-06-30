6e09c1dd69d507585b534e2b88825cae38267533
Keyur Kamdar <keyur-kamdar@users.noreply.github.com>
2017-06-29 17:33:39 -0700

    Update Properties.java

--- src/csulb/edu/SearchEngine/Classes/Properties.java
+++ src/csulb/edu/SearchEngine/Classes/Properties.java
@@ -4,17 +4,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Properties {
-
-	
-	
-	public static Path library_Directory_Path=Paths.get("D:\\Keyur\\CSULB\\Sem-3(Fall-2014)\\Search Engine Technology\\MileStone-2\\angels_corpus2\\angels3").toAbsolutePath();
-	
+	public static Path library_Directory_Path=Paths.get("D:\\Keyur\\CSULB\\Sem-3(Fall-2014)\\Search Engine Technology\\MileStone-2\\angels_corpus2\\angels3").toAbsolutePath();	
 	public static boolean Lib=true;
 	public static boolean VariableLib=true;
 	public static boolean ImpactOrderLib=true;
 	public static boolean VariableByteEncode=false;
 	public static boolean NoEncode=true;
-	public static boolean Impactorder=false;
-	
-	
+	public static boolean Impactorder=false;	
 }
